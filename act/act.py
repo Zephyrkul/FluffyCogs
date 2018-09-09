f9bb6d8d7c42bfc5e30bae60b846c9f89571f1f6
zephyrkul <zephyrkul@users.noreply.github.com>
2018-09-08 20:59:27 -0600

    act parsing (#3)

--- act/act.py
+++ act/act.py
@@ -1,22 +1,21 @@
 import discord
-import inflect
+import inflection
+import itertools
+import re
 from typing import Union
 
 from redbot.core import commands
-from redbot.core.i18n import Translator, cog_i18n
+from redbot.core.utils.chat_formatting import italics
+
+from .helpers import *
 
 
-_ = Translator("Act", __name__)
-
-
-@cog_i18n(_)
 class Act:
 
     __author__ = "Zephyrkul"
 
     def __init__(self, bot):
         self.bot = bot
-        self.engine = inflect.engine()
 
     @commands.command(hidden=True)
     async def act(self, ctx, *, target: Union[discord.Member, str] = None):
@@ -25,17 +24,37 @@ class Act:
         """
         if not target or isinstance(target, str):
             return  # no help text
-        action = ctx.invoked_with
-        if not self.engine.singular_noun(action):
-            action = self.engine.plural_noun(action)
-        await ctx.send(f"*{action} {target.mention}*")
+
+        action = inflection.humanize(ctx.invoked_with).split()
+        iverbs = []
+
+        for cycle in range(2):
+            if iverbs:
+                break
+            for i, act in enumerate(action):
+                act = act.lower()
+                if (
+                    act in NOLY_ADV
+                    or act in CONJ
+                    or (act.endswith("ly") and act not in LY_VERBS)
+                    or (not cycle and act in SOFT_VERBS)
+                ):
+                    continue
+                action[i] = inflection.pluralize(action[i])
+                iverbs.append(i)
+
+        if not iverbs:
+            return
+        i = max(iverbs)
+        action.insert(i + 1, target.mention)
+        await ctx.send(italics(" ".join(action)))
 
     async def on_message(self, message):
         if message.author.bot:
             return
 
         ctx = await self.bot.get_context(message)
-        if ctx.prefix is None:
+        if ctx.prefix is None or not re.match(r"^\w+$", ctx.invoked_with):
             return
 
         if ctx.valid and ctx.command.enabled:
