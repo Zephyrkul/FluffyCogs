ecb5ceccd80dc518073b3bbf911e1f745c07f0c7
zephyrkul <zephyrkul@users.noreply.github.com>
2019-01-19 12:13:31 -0700

    [nationstates] clarify import error

--- nationstates/__init__.py
+++ nationstates/__init__.py
@@ -2,6 +2,13 @@ from .nationstates import NationStates
 
 
 async def setup(bot):
+    try:
+        import sans
+    except ImportError as e:
+        raise RuntimeError(
+            "The sans library is not installed.\n"
+            "Run this command to install it: [p]pipinstall sans"
+        ) from e
     cog = NationStates(bot)
     await cog.initialize()
     bot.add_cog(cog)
