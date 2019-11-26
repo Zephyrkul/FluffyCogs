ffa0425a3d96cbcadd9cbeb0d20a78b2c5ed8cb7
zephyrkul <zephyrkul@users.noreply.github.com>
2019-11-26 16:51:52 -0700

    [ns] better sans versioning check

--- nationstates/__init__.py
+++ nationstates/__init__.py
@@ -1,21 +1,18 @@
-from .nationstates import NationStates
+try:
+    import sans
+    from .nationstates import NationStates
+    import_failed = None
+except ImportError as e:
+    import_failed = e
 
 
 async def setup(bot):
-    try:
-        import sans
-    except ImportError as e:
+    if import_failed or sans.version_info < type(sans.version_info)("0.0.1b6"):
         raise RuntimeError(
-            "The sans library is not installed.\n"
-            "Run this command to install it: [p]pipinstall sans"
-        ) from e
-    else:
-        if sans.version_info < type(sans.version_info)("0.0.1b6"):
-            raise RuntimeError(
-                "The sans library is out of date.\n"
-                "Run this command to update it: [p]pipinstall sans\n"
-                "You may have to [p]restart your bot to have the new version take effect."
-            )
+            "The sans library is out of date or not installed.\n"
+            "Run this command to update it: [p]pipinstall sans\n"
+             "You may have to [p]restart your bot to have the new version take effect."
+        ) from import_failed
     cog = NationStates(bot)
     await cog.initialize()
     bot.add_cog(cog)
