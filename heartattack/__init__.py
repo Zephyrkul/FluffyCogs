ce19d66e5db95fc18d4f28dfd7f1157b3f2e1bd8
zephyrkul <zephyrkul@users.noreply.github.com>
2020-07-17 10:05:34 -0600

    [heartattack] deprecate for d.py 1.4
    d.py 1.4 has a traceback built-in for heartattack issues

--- heartattack/__init__.py
+++ heartattack/__init__.py
@@ -1,5 +1,9 @@
+import discord
+from redbot.core.errors import CogLoadError
 from .heartattack import HeartAttack
 
 
 def setup(bot):
+    if discord.version_info > (1, 4):
+        raise CogLoadError("Heartattack is no longer necessary in d.py 1.4+")
     bot.add_cog(HeartAttack())
