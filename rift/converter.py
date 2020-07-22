5b26bdb5aba2925449f85a727ce499794611f8d9
zephyrkul <zephyrkul@users.noreply.github.com>
2020-07-22 17:56:48 -0600

    Rift rework (#18)
    * [rift] initial rework
    
    * [rift] fix Limited bug
    
    * style

--- rift/converter.py
+++ rift/converter.py
@@ -1,147 +1,121 @@
 import asyncio
 import logging
-from collections import namedtuple
-from contextlib import suppress
+from itertools import chain, filterfalse
+from typing import List
 
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import pagify
+from redbot.core.i18n import Translator
+from redbot.core.utils.predicates import MessagePredicate
+from urlnorm import norm_netloc
 
 log = logging.getLogger("red.fluffy.rift.converter")
+_ = Translator(__name__, __file__)
 
 
-Rift = namedtuple("Rift", ["author", "source", "destination"])
-Rift.__str__ = lambda self: f"{self.author}: {self.source} ► {self.destination}"
+class Limited(discord.abc.Messageable):
+    def __init__(self, *, message: discord.Message):
+        self.message = message
 
-
-def search_converter(translator):
-    def convert(argument):
+    def __getattr__(self, attr):
         try:
-            return [("author", "source", "destination").index(argument.lower())]
-        except ValueError:
-            raise commands.BadArgument(
-                translator("Argument must be one of author, source, or destination.")
-            )
+            return getattr(self.message.channel, attr)
+        except AttributeError:
+            return getattr(self.message, attr)
 
-    return convert
+    def __hash__(self) -> int:
+        return hash((self.message.author, self.message.channel))
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Limited):
+            return NotImplemented
+        return (self.message.author, self.message.channel) == (o.message.author, o.message.channel)
+
+    def __str__(self) -> str:
+        return f"{self.message.author}, in {self.message.channel}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(message={self.message!r})"
+
+    def _get_channel(self):
+        return self.message.channel._get_channel()
 
 
-class RiftConverter(commands.Converter):
-    def __init__(self, translator, *, globally=False):
-        self.globally = globally
-        self.translator = translator
-
-    async def convert(self, ctx, argument):
-        _ = self.translator
-        destination = await self.search(ctx, argument, self.globally, _)
-        if len(destination) > 1:
-
-            def check(message):
-                nonlocal destination
-                if message.author != ctx.author or message.channel != ctx.channel:
-                    return False
-                with suppress(Exception):
-                    destination = [destination[int(message.content)]]
-                    return True
-                return False
-
-            message = _("Multiple results found. Choose a destination:")
-            message += "\n\n" + "\n".join(
-                f"{i}: {d} ({getattr(d, 'guild')})" if hasattr(d, "guild") else f"{i}: {d}"
-                for i, d in enumerate(destination)
-            )
-            for page in pagify(message):
-                await ctx.send(page)
-            try:
-                await ctx.bot.wait_for("message", check=check)
-            except asyncio.TimeoutError as e:
-                raise commands.BadArgument(_("Never mind, then.")) from e
-        result = destination[0]
-        destination = ctx.bot.get_channel(result.id) or ctx.bot.get_user(result.id)
-        if not destination:
-            raise commands.BadArgument(_("I don't have access to {} anymore.").format(result))
-        source = ctx.channel if isinstance(ctx.channel, discord.TextChannel) else ctx.author
-        rift = Rift(author=ctx.author, source=source, destination=destination)
-        if rift in ctx.cog.open_rifts:
-            raise commands.BadArgument(_("This rift already exists."))
-        return rift
+class DiscordConverter(commands.Converter):
+    @classmethod
+    async def convert(
+        cls, ctx, argument: str, *, globally: bool = True
+    ) -> discord.abc.Messageable:
+        results = await cls.search(ctx, argument, globally=globally)
+        if len(results) == 0:
+            m = _("No destinations found.")
+            await ctx.send(m)
+            raise commands.BadArgument(m)
+        if len(results) == 1:
+            return results[0]
+        message = _("Multiple results found. Choose a destination:\n\n")
+        for i, result in enumerate(results):
+            m = f"{i}: {result} ({result.id})"
+            if guild := getattr(result, "guild", None):
+                m = f"{m}, in {guild}"
+            message = f"{message}\n{m}"
+        await ctx.send(message)
+        predicate = MessagePredicate.less(len(results), ctx=ctx)
+        try:
+            await ctx.bot.wait_for("message", check=predicate, timeout=30)
+        except asyncio.TimeoutError as te:
+            m = _("No destination selected.")
+            await ctx.send(m)
+            raise commands.BadArgument(m)
+        return results[predicate]
 
     @classmethod
-    async def search(cls, ctx, argument, globally, _):
+    async def search(
+        cls, ctx, argument: str, *, globally: bool = False
+    ) -> List[discord.abc.Messageable]:
         is_owner = await ctx.bot.is_owner(ctx.author)
+        globally = globally or is_owner
+        if not globally and not ctx.guild:
+            return []
+        source = ctx.channel if ctx.guild else ctx.author
         config = ctx.cog.config
-        guilds = ctx.bot.guilds.copy() if is_owner or globally else [ctx.guild]
-        result = set()
+        blacklists = await asyncio.gather(
+            config.all_guilds(), config.all_channels(), config.all_users()
+        )
+        if globally:
+            guilds = ctx.bot.guilds
+        else:
+            guilds = [ctx.guild]
+        results = set()
         for guild in guilds:
-            if not await cls.guild_filter(ctx, is_owner, guild):
+            if blacklists[0].get(guild.id, {}).get("blacklisted"):
                 continue
-            for channel in guild.text_channels.copy():
-                if cls.channel_filter(ctx, is_owner, channel, argument):
-                    if not await config.channel(channel).blacklisted():
-                        result.add(channel)
-                    else:
-                        log.debug("Channel %s ignored: blacklisted", channel.id)
-            for member in guild.members.copy():
-                if member not in result and cls.user_filter(ctx, is_owner, member, argument):
-                    if not await config.user(member).blacklisted():
-                        result.add(member)
-                    else:
-                        log.debug("User %s ignored: blacklisted", member.id)
-        if not result:
-            raise commands.BadArgument(
-                _(
-                    "Destination {!r} not found. Either I don't have access or it has been blacklisted."
-                ).format(argument)
-            )
-        return list(result)
-
-    @staticmethod
-    async def guild_filter(ctx, is_owner, guild):
-        if await ctx.cog.config.guild(guild).blacklisted():
-            log.debug("Guild %s ignored: blacklisted", guild.id)
-            return False
-        if guild == ctx.guild:
-            return True
-        return is_owner or guild.get_member(ctx.author.id)
-
-    @staticmethod
-    def channel_filter(ctx, is_owner, channel, argument):
-        if ctx.channel == channel:
-            log.debug("Channel %s ignored: it is the current channel", channel.id)
-            return False
-        bot = channel.guild.me
-        if not channel.permissions_for(bot).send_messages:
-            log.debug("Channel %s ignored: the bot doesn't have access", channel.id)
-            return False
-        if not is_owner:
-            member = channel.guild.get_member(ctx.author.id)
-            if not channel.permissions_for(member).send_messages:
-                log.debug("Channel %s ignored: user %s doesn't have access", channel.id, member.id)
-                return False
-        if argument == str(channel.id):
-            return True
-        if argument == f"<#{channel.id}>":
-            return True
-        if argument == channel.name:
-            return True
-        return False
-
-    @staticmethod
-    def user_filter(ctx, is_owner, member, argument):
-        if ctx.author.id == member.id and isinstance(ctx.channel, discord.DMChannel):
-            log.debug("User %s ignored: it is the current channel", member.id)
-            return False
-        if member.bot:
-            log.debug("User %s ignored: it is a bot", member.id)
-            return False
-        if argument == str(member.id):
-            return True
-        if argument in (f"<@{member.id}>", f"<@!{member.id}>"):
-            return True
-        if argument == f"{member.name}#{member.discriminator}":
-            return True
-        if argument == member.name:
-            return True
-        if ctx.guild == member.guild and argument == member.display_name:
-            return True
-        return False
+            for channel in guild.text_channels:
+                if channel == source:
+                    continue
+                if channel in results:
+                    continue
+                if blacklists[1].get(channel.id, {}).get("blacklisted"):
+                    continue
+                if argument.lstrip("#") in (str(channel.id), channel.mention, channel.name):
+                    results.add(channel)
+            for user in guild.members:
+                if user == source:
+                    continue
+                if user.bot:
+                    continue
+                if user in results:
+                    continue
+                if blacklists[2].get(user.id, {}).get("blacklisted"):
+                    continue
+                to_match = [str(user.id), f"<@{user.id}>", f"<@!{user.id}>", str(user), user.name]
+                if guild == ctx.guild:
+                    to_match.append(user.display_name)
+                if argument.lstrip("@") not in to_match:
+                    continue
+                if not await ctx.bot.allowed_by_whitelist_blacklist(user):
+                    continue
+                results.add(ctx.bot.get_user(user.id))
+            await asyncio.sleep(0)
+        results.discard(None)
+        return list(results)
