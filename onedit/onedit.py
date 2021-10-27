a074b48395710227aaa253b496c9864552504694
zephyrkul <zephyrkul@users.noreply.github.com>
2021-10-27 11:15:43 -0600

    [onedit] support cog disabling, support Fox-CCRole

--- onedit/onedit.py
+++ onedit/onedit.py
@@ -1,6 +1,8 @@
 import asyncio
+from typing import Optional
 
 import discord
+from redbot.core.bot import Red
 from redbot.core import Config, checks, commands, i18n
 
 
@@ -11,19 +13,20 @@ class OnEdit(commands.Cog):
     async def red_delete_data_for_user(self, *, requester, user_id):
         pass  # No data to delete
 
-    def __init__(self, bot):
+    def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=2_113_674_295, force_registration=True)
         self.config.register_global(timeout=5)
-        self.timeout = None
+        self.timeout: Optional[float] = None
 
     async def edit_process_commands(self, message: discord.Message):
         """Same as Red's method (Red.process_commands), but dont dispatch message_without_command."""
         if not message.author.bot:
-            ctx = await self.bot.get_context(message)
+            ctx: commands.Context = await self.bot.get_context(message)
             await self.bot.invoke(ctx)
             if ctx.valid is False:
-                for allowed_name in ("Alias", "CustomCommands"):
+                # My Act and Phen's Tags use on_command_error, and thus aren't needed in this list.
+                for allowed_name in ("Alias", "CustomCommands", "CCRoles"):
                     if listener := getattr(
                         self.bot.get_cog(allowed_name), "on_message_without_command", None
                     ):
@@ -31,7 +34,7 @@ class OnEdit(commands.Cog):
 
     @commands.command()
     @checks.is_owner()
-    async def edittime(self, ctx, *, timeout: float):
+    async def edittime(self, ctx: commands.Context, *, timeout: float):
         """
         Change how long the bot will listen for message edits to invoke as commands.
 
@@ -44,15 +47,16 @@ class OnEdit(commands.Cog):
         await ctx.tick()
 
     @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
         if not after.edited_at:
             return
         if before.content == after.content:
+            return
+        if await self.bot.cog_disabled_in_guild(self, after.guild):
             return
         if self.timeout is None:
             self.timeout = await self.config.timeout()
         if (after.edited_at - after.created_at).total_seconds() > self.timeout:
             return
-        if method := getattr(i18n, "set_contextual_locales_from_guild"):
-            await method(self.bot, after.guild)
+        await i18n.set_contextual_locales_from_guild(self.bot, after.guild)
         await self.edit_process_commands(after)
