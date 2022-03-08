f58d28058de77672b5eedc6cb4f9487b539c1746
zephyrkul <zephyrkul@users.noreply.github.com>
2022-03-07 20:41:19 -0700

    [ots] prevent typeerrors on send

--- onetrueslash/channel.py
+++ onetrueslash/channel.py
@@ -1,6 +1,15 @@
+import inspect
 from typing import Optional
 
+import discord
+
 from .utils import Thinking, contexts
+
+INCOMPATABLE_PARAMETERS_DISCARD = frozenset(
+    k
+    for k in inspect.signature(discord.abc.Messageable.send).parameters
+    if k not in inspect.signature(discord.Webhook.send).parameters
+)
 
 
 class InterChannel:
@@ -9,21 +18,23 @@ class InterChannel:
     async def send(self, *args, **kwargs):
         ctx = contexts.get()
         interaction = ctx.interaction
-        await self.trigger_typing(ephemeral=kwargs.pop("ephemeral", None))
+        await self.trigger_typing(_ephemeral=kwargs.pop("ephemeral", None))
         delete_after = kwargs.pop("delete_after", None)
         kwargs["wait"] = True
+        for key in INCOMPATABLE_PARAMETERS_DISCARD:
+            kwargs.pop(key, None)
         m = await interaction.followup.send(*args, **kwargs)
         if delete_after is not None:
             await m.delete(delay=delete_after)
         return m
 
-    async def trigger_typing(self, *, ephemeral: Optional[bool] = None) -> None:
+    async def trigger_typing(self, *, _ephemeral: Optional[bool] = None) -> None:
         ctx = contexts.get()
         if not ctx._deferred:
             ctx._deferred = True
-            if ephemeral is None:
+            if _ephemeral is None:
                 ephemeral = ctx.command_failed or not ctx.command
-            await ctx.interaction.response.defer(ephemeral=ephemeral)
+            await ctx.interaction.response.defer(ephemeral=_ephemeral)
 
     def typing(self):
         return Thinking(self)
