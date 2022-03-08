cc5e46cf83f779c05c95bf48261d10763711b146
zephyrkul <zephyrkul@users.noreply.github.com>
2022-03-07 22:25:21 -0700

    [ots] remove dev code, delay unsync

--- onetrueslash/__init__.py
+++ onetrueslash/__init__.py
@@ -6,7 +6,6 @@ try:
 except ImportError:
     import re
 
-import discord
 from discord import app_commands
 from redbot.core import commands as red_commands
 from redbot.core.bot import Red
@@ -36,22 +35,15 @@ async def _setup(bot: Red):
     assert isinstance(bot.tree, app_commands.CommandTree)
     await bot.wait_until_red_ready()
     assert bot.user
-    if bot.user.id == 256505473807679488:
-        guild = discord.Object(id=133049272517001216)
-    else:
-        guild = None
     onetrueslash.name = re.sub(r"[^\w-]+", "_", bot.user.name.casefold())
-    bot.tree.add_command(onetrueslash, guild=guild)
-    await bot.tree.sync(guild=guild)
+    bot.tree.add_command(onetrueslash, guild=None)
+    await bot.tree.sync(guild=None)
 
 
 def teardown(bot: Red):
     bot.remove_before_invoke_hook(before_hook)
     if bot.user:
         assert isinstance(bot.tree, app_commands.CommandTree)
-        if bot.user.id == 256505473807679488:
-            guild = discord.Object(id=133049272517001216)
-        else:
-            guild = None
-        bot.tree.remove_command(onetrueslash.name, guild=guild)
-        asyncio.create_task(bot.tree.sync(guild=guild))
+        bot.tree.remove_command(onetrueslash.name, guild=None)
+        # delay the slash removal a bit in case this is a reload
+        asyncio.get_event_loop().call_later(2, asyncio.create_task, bot.tree.sync(guild=None))
