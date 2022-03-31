2b1f3b86f80760bc3203729d121f6687343e3dd4
zephyrkul <zephyrkul@users.noreply.github.com>
2022-03-31 14:00:04 -0600

    [ots] update onetrueslash name on username update

--- onetrueslash/__init__.py
+++ onetrueslash/__init__.py
@@ -11,7 +11,7 @@ from redbot.core.bot import Red
 from redbot.core.errors import CogLoadError
 
 from .commands import onetrueslash
-from .events import before_hook
+from .events import before_hook, on_user_update
 
 LOG = logging.getLogger("red.fluffy.onetrueslash")
 
@@ -20,6 +20,7 @@ async def setup(bot: Red) -> None:
     if not hasattr(discord, "app_commands"):
         raise CogLoadError("This cog requires the latest discord.py 2.0.0a.")
     bot.before_invoke(before_hook)
+    bot.add_listener(on_user_update)
     bot.add_dev_env_value("interaction", lambda ctx: getattr(ctx, "interaction", None))
     asyncio.create_task(_setup(bot))
 
@@ -44,6 +45,7 @@ async def _setup(bot: Red):
 
 async def teardown(bot: Red):
     bot.remove_before_invoke_hook(before_hook)
+    bot.remove_listener(on_user_update)
     bot.remove_dev_env_value("interaction")
 
     bot.tree.remove_command(onetrueslash.name, guild=None)
--- onetrueslash/events.py
+++ onetrueslash/events.py
@@ -1,8 +1,16 @@
-from typing import Optional
 import logging
+from typing import Optional
+
+try:
+    import regex as re
+except ImportError:
+    import re
 
 import discord
 from redbot.core import commands as red_commands
+from redbot.core.bot import Red
+
+from .commands import onetrueslash
 
 LOG = logging.getLogger("red.fluffy.onetrueslash.events")
 
@@ -12,3 +20,22 @@ async def before_hook(ctx: red_commands.Context):
     if (interaction := getattr(ctx, "interaction", None)) and not interaction.response.is_done():
         ctx._deferring = True  # type: ignore
         await interaction.response.defer(ephemeral=False)
+
+
+async def on_user_update(before: discord.User, after: discord.User):
+    bot: Red = after._state._get_client()  # type: ignore # DEP-WARN
+    assert bot.user
+    if after.id != bot.user.id:
+        return
+    if before.name == after.name:
+        return
+    bot.tree.remove_command(onetrueslash.name)
+    onetrueslash.name = re.sub(r"[^\w-]+", "-", bot.user.name.casefold())
+    bot.tree.add_command(onetrueslash, guild=None)
+    await bot.send_to_owners(
+        "The bot's username has changed. onetrueslash's slash command has been updated to reflect this.\n"
+        "It is recommended not to change the bot's name too often with this cog, as this can potentially "
+        "create confusion for users as well as ratelimiting issues for the bot."
+    )
+    LOG.debug("Syncing slash tree in response to username change.")
+    await bot.tree.sync(guild=None)
