f7e10f5f261b6a8163647e3ae40c5a908640e28b
zephyrkul <zephyrkul@users.noreply.github.com>
2022-04-04 18:20:58 -0600

    [ots] fix `arguments:` in help w/o args

--- onetrueslash/commands.py
+++ onetrueslash/commands.py
@@ -40,7 +40,7 @@ async def onetrueslash(
         actual_command: Optional[commands.Command] = None
         if arguments:
             actual_command = interaction.client.get_command(arguments)
-            if actual_command:
+            if actual_command and (signature := actual_command.signature):
                 actual_command = copy(actual_command)
                 actual_command.usage = f"arguments: {actual_command.signature}"
         await interaction.client.send_help_for(
